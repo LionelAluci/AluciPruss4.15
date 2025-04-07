a310716ac198301a55331f7cadd5854fb7920d79
AluciPruss <lionelaluci9@gmail.com>
2025-04-07 16:27:50 -0300

    Add files via upload

---
+++ gg7.js
@@ -0,0 +1,11 @@
+var a=prompt("ingrese un palabra")
+if(a===a.toUpperCase()){
+    console.log("mayuscula")
+}
+else if(a===a.toLowerCase()){
+    console.log("minuscula")
+}
+else {
+    console.log("ambas")
+}
+
