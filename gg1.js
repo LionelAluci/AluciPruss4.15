3de762dc29ac8d4251c3b7c262adc6e83d1523b1
AluciPruss <lionelaluci9@gmail.com>
2025-04-07 13:59:49 -0300

    Add files via upload

---
+++ gg1.js
@@ -0,0 +1,6 @@
+function button(){
+var meses=["enero", "febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+if(button){
+    console.log(meses)
+}
+}
